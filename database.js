b5b954a65988567a9ad1db6108fe2603fa8a74f2
Yashwant937363 <yashwantpoyrekar@gmail.com>
2023-12-24 18:53:44 +0530

    some consoles cleared

--- database.js
+++ database.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const url = process.env.DATABASE_URL;
 
 const connectToDatabase = () => {
-    console.log(url);
     mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
     const db = mongoose.connection;
