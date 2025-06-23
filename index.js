e14c4ce39e7f9bb317ad69909744fdb37d7c7c25
Yashwant937363 <yashwantpoyrekar@gmail.com>
2025-06-23 23:31:51 +0530

    second version for branch tailwind-ts

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express');
-const connectToDatabase = require('./database');
-const cors = require('cors');
+const express = require("express");
+const connectToDatabase = require("./database");
+const cors = require("cors");
 connectToDatabase();
 
 const app = express();
 const port = process.env.PORT || 5000;
+const frontendurl = process.env.FRONTENDURL || "http://localhost:3000/";
+// 'https://todo-list-by-yashwant.netlify.app'
 app.get("/", (req, res) => {
-    res.status(200).send('Hello World');
+  res.status(200).send("Hello World");
 });
 
-app.use(cors({
-    origin: 'https://todo-list-by-yashwant.netlify.app',
-    credentials: true, 
-}));
+app.use(cors({}));
 
 app.use(express.json());
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
-    console.log("server running at ", port);
+  console.log("server running at ", port);
 });
--- models/Note.js
+++ models/Note.js
@@ -1,23 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const notesModel = new Schema({
+const notesModel = new Schema(
+  {
     user: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'user'
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
     },
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     body: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     tag: {
-        type: String,
-        default: "General",
+      type: String,
+      default: "General",
     },
-}, { timestamps: true },)
-const Notes = mongoose.model('notes', notesModel);
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__V;
+        return ret;
+      },
+    },
+  }
+);
+const Notes = mongoose.model("notes", notesModel);
 module.exports = Notes;
